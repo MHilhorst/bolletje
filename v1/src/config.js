11ec1aa0866ac395da5bf6f8a884e501fb091ab2
Ricy <m.j.hilhorst@students.uu.nl>
2020-04-14 16:29:29 +0200

    updates host url

--- v1/src/config.js
+++ v1/src/config.js
@@ -1,10 +1,10 @@
 const config = {
-  host: "http://localhost:8000",
+  host: 'http://app.snapse.nl:8000/',
   bol: {
-    clientId: "d061ce7d-ae6c-41fa-af56-924b270a8ecd",
+    clientId: 'd061ce7d-ae6c-41fa-af56-924b270a8ecd',
     secret:
-      "ALmUdWW2iPa9tz5vnsI2uHkmaO0JRLr8xG6RvU9tthWODq5UfJGEoQgU1zoWtQIoD6w0V-BRop8XmXmw782Yoy4"
-  }
+      'ALmUdWW2iPa9tz5vnsI2uHkmaO0JRLr8xG6RvU9tthWODq5UfJGEoQgU1zoWtQIoD6w0V-BRop8XmXmw782Yoy4',
+  },
 };
 
 export default config;
